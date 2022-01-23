02e7f5c521bf2956d2b65cc5709c96b90702039b
Diego Do Santos <ddosantos@hotmail.com>
2022-01-23 18:09:18 -0300

    Added responsive to Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { Twitter, Mail, Instagram, ArrowRight } from "react-feather";
+// Components
+import { Navbar } from "./components/Navbar";
+// Icons
+import { ArrowRight } from "react-feather";
 
 const services = [
   "Design",
@@ -12,57 +15,51 @@ function App() {
   return (
     <>
       <div className="min-h-screen bg-gradient-to-br from-[#0C1F5A] via-[#06102D] to-[#0C1F5A] flex flex-col">
-        {/* Navbar */}
-        <nav className="w-full py-7 flex md:flex-col items-center fixed">
-          <div className="container mx-auto px-4 text-center relative">
-            <h2 className="text-2xl text-[#D2DCF9] uppercase">
-              Creative Studio
-            </h2>
-            <div className="flex items-center gap-8 absolute top-0 bottom-0 right-0 text-[#7895ED]">
-              <Instagram />
-              <Twitter />
-              <Mail />
-            </div>
-          </div>
-        </nav>
-
+        <Navbar />
         {/* Hero */}
-        <div className="container mx-auto px-4 text-center flex flex-col justify-center flex-grow z-0">
+        <div className="xl:container mx-auto px-4 flex flex-col flex-grow justify-center items-center text-center z-0">
           {/* Hero Text */}
-          <div className="text-7xl font-bold uppercase mb-5">
+          <div className="text-6xl md:text-6xl lg:text-7xl font-bold uppercase mt-20 mb-5 relative">
             <h1 className="text-[#7895ED] antialiased">
-              Take care of the content <br />
-              <span className="text-[#D2DCF9]">We tell the story</span>
+              Take care of the content
+              <span className="block text-[#D2DCF9]">We tell the story</span>
             </h1>
+            {/* <img
+              src="./images/prism1.png"
+              className="absolute bottom-1/4 right-[18%] w-96 -rotate-[20deg] floating drop-shadow-xl pointer-events-none"
+            />
+            <img
+              src="./images/prism2.png"
+              className="absolute top-[20%] left-[25%] w-40 floating drop-shadow-xl pointer-events-none"
+            /> */}
           </div>
-          <p className="text-2xl text-[#D2DCF9] mb-16">
+          <p className="text-xl lg:text-2xl text-[#D2DCF9] mb-16">
             We are a Creative Studio based in Uruguay.
           </p>
 
           {/* Hero CTA */}
-          <a className="flex items-center gap-2 px-6 py-3 bg-[#51E075] hover:bg-[#83e29b] text-[#06102D] uppercase self-center font-semibold text-xl leading-5 cursor-pointer transition-all">
-            See our work <ArrowRight />
-          </a>
-
-          <img
-            src="./images/prism1.png"
-            className="absolute bottom-1/4 right-[18%] w-96 -rotate-[20deg] floating drop-shadow-xl pointer-events-none"
-          />
-          <img
-            src="./images/prism2.png"
-            className="absolute top-[20%] left-[25%] w-40 floating drop-shadow-xl pointer-events-none"
-          />
+          <div className="flex flex-col sm:flex-row gap-6 w-full justify-center">
+            <a className="flex items-center justify-center gap-2 px-6 py-3 bg-[#51E075] border border-[#51E075] hover:bg-[#83e29b] hover:border-[#83e29b] text-[#06102D] uppercase sm:self-center font-semibold text-xl leading-5 cursor-pointer transition-all">
+              See Our Work <ArrowRight />
+            </a>
+            <a className="flex items-center justify-center px-6 py-3 text-[#7895ED] border border-[#7895ED] uppercase font-semibold text-xl leading-6 cursor-pointer transition-all">
+              Contact Us
+            </a>
+          </div>
 
           {/* Services Carousel */}
-          <div className="absolute bottom-20 -z-10 pointer-events-none flex">
+          {/* <div className="hidden relative bottom-24 -z-10 pointer-events-none lg:flex overflow-hidden">
             <div className="flex gap-48 text-[150px] text-[#0c1f5a54] leading-none uppercase whitespace-nowrap font-[Bauziet] animate-translate">
               {services.map((service) => (
                 <span>{service}</span>
               ))}
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
+      <section className="min-h-screen bg-gray-900">
+        <p>Prueba</p>
+      </section>
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Twitter, Mail, Instagram, Menu, Globe } from "react-feather";
+
+export function Navbar() {
+  return (
+    <nav className="w-full py-5 md:py-7 flex items-center fixed top-0 bg-[#06102D80] backdrop-blur-md md:bg-transparent z-10">
+      <div className="w-full flex justify-between sm:justify-center xl:container lg:container md:container sm:container mx-auto px-4 relative">
+        {/* Menu */}
+        <div className="sm:hidden text-[#7895ED]">
+          <Menu />
+        </div>
+        {/* Logo */}
+        <h2 className="text-lg md:text-2xl text-[#D2DCF9] uppercase">
+          Creative Studio
+        </h2>
+        {/* Social Icons */}
+        <div className="hidden lg:flex items-center gap-8 absolute top-0 right-0 lg:right-4 text-[#7895ED]">
+          <Instagram />
+          <Twitter />
+          <Mail />
+        </div>
+        {/* Language */}
+        <div className="sm:hidden text-[#7895ED]">
+          <Globe />
+        </div>
+      </div>
+    </nav>
+  );
+}
