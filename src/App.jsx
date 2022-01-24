5eed5828b41527323b28750d63aad3f5d539bbac
Diego Do Santos <ddosantos@hotmail.com>
2022-01-24 20:24:06 -0300

    Fixed Marquee & CTA on desktop

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { ArrowRight } from "react-feather";
 import Marquee from "react-fast-marquee";
 
 const services = [
-  "Design",
+  " • Design",
   "Web Development",
   "Branding",
   "Video Production",
@@ -42,19 +42,19 @@ function App() {
 
           {/* Hero CTA */}
           <div className="flex flex-col sm:flex-row gap-4 w-full justify-end sm:justify-center grow mb-16 sm:m-0">
-            <a className="flex items-center justify-center gap-2 px-6 py-3 bg-[#51E075] border border-[#51E075] hover:bg-[#83e29b] hover:border-[#83e29b] text-[#06102D] uppercase sm:self-center font-semibold text-xl leading-5 cursor-pointer transition-all">
+            <a className="flex items-center justify-center md:self-start gap-2 px-6 py-3 bg-[#51E075] border border-[#51E075] hover:bg-[#83e29b] hover:border-[#83e29b] text-[#06102D] uppercase sm:self-center font-semibold text-xl leading-5 cursor-pointer transition-all">
               See Our Work <ArrowRight />
             </a>
-            <a className="flex items-center justify-center px-6 py-3 text-[#7895ED] border border-[#7895ED] uppercase font-semibold text-xl leading-6 cursor-pointer transition-all sm:self-center">
+            <a className="flex items-center justify-center md:self-start px-6 py-3 text-[#7895ED] border border-[#7895ED] uppercase font-semibold text-xl leading-6 cursor-pointer transition-all sm:self-center">
               Contact Us
             </a>
           </div>
         </div>
-        <div className="relative hidden lg:block pointer-events-none -z-0 bottom-8">
+        <div className="absolute top-[290px] -left-[250px] w-[150%] rotate-[335deg] hidden lg:block pointer-events-none -z-0">
           <Marquee
             gradient={false}
             speed={80}
-            className="text-[150px] text-[#0c1f5a54] leading-none uppercase whitespace-nowrap font-[Bauziet] overflow-hidden "
+            className="text-[150px] text-[#0c1f5a31] leading-none uppercase whitespace-nowrap font-[Bauziet] overflow-hidden "
           >
             {servicesStr}
           </Marquee>
