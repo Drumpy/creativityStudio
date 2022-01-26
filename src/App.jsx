1f64a6a73ebc8bbabe3cb050d097d86207dc6a08
Diego Do Santos <ddosantos@hotmail.com>
2022-01-26 16:16:39 -0300

    Added cursor animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { MarqueeServices } from "./components/MarqueeServices";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
     <>
       <div className="min-h-screen bg-gradient-to-br from-[#0c1f5a] via-[#06102D] to-[#0C1F5A] flex flex-col">
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={20}
+          color="81, 224, 117"
+          outerAlpha={0.2}
+          innerScale={0.5}
+          outerScale={3}
+        />
         <Navbar />
         <Hero />
         <MarqueeServices />
